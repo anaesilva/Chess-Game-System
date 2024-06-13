e6fc01994c826a6fbe124bda8dc1ffecf6b18c97
Ana Silva <aeusebio@uolinc.com>
2024-06-13 18:17:57 -0300

    Starting to implement Board and Piece

---
+++ src/main/java/boardgame/Position.java
@@ -0,0 +1,20 @@
+package boardgame;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Position {
+    private int row;
+    private int column;
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+}
